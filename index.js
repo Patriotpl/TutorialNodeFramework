08716d59485f6129d85415c17063650b76db7aa4
Patriotpl <patriotpl22@gmail.com>
2024-08-23 02:55:42 +0200

    lesson 1 - process and console

---
+++ index.js
@@ -0,0 +1,12 @@
+process.stdout.write("learning node");
+
+console.log(process.version);
+console.log(process.platform);
+console.log(process.uptime());
+console.log(process.cpuUsage());
+console.log(process.resourceUsage().systemCPUTime);
+console.log(process.memoryUsage());
+console.log(process.cwd());
+console.log(process.title);
+console.log(process.argv);
+console.log(process.pid);
